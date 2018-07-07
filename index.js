1d9fd08c9d6d20479dc1a415b94f054391abc419
rahmeen <rahmeenwill99@gmail.com>
2018-07-07 19:42:45 +0530

    Resolved BAD_REQUEST error. The skeleton bot currently logs the any comment on any issue of the repository it's installed on

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports = robot => {
-  robot.log('Yay, the app was loaded!')
- robot.on('issues.opened', async context => {
+module.exports = app => {
+  app.log('Yay, the app was loaded!')
+ app.on('issue_comment', async context => {
    // `context` extracts information from the event, which can be passed to
    // GitHub API calls. This will return:
    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-  context.log(context.payload)
+  context.log(context.payload.comment.body)
  })
 }
+
