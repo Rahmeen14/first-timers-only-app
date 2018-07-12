1a153213e83bd519c8b5f2ed05070b9e8603b803
Akshita <aggarwalakshita1@gmail.com>
2018-07-12 23:44:59 +0530

    add standard

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   app.log('Yay, the app was loaded!')
-  app.on("issue_comment", async context => {
+  app.on('issue_comment', async context => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
