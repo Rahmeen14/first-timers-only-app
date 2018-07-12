cf54f9aaf522ce7eb09d4d47e1eee9c544f210b0
Akshita <aggarwalakshita1@gmail.com>
2018-07-12 22:58:23 +0530

    check for first-timers comment

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 module.exports = app => {
-  app.log('Yay, the app was loaded!')
- app.on('issue_comment', async context => {
-   // `context` extracts information from the event, which can be passed to
-   // GitHub API calls. This will return:
-   //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-  context.log(context.payload.comment.body)
- })
-}
+  app.log("Yay, the app was loaded!");
+  app.on("issue_comment", async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
 
+    const s = "Can I work on this?";
+    const t = "first-timers-only";
+    let check = false;
+    let labelArray = context.payload.issue.labels;
+    if (context.payload.comment.body == s) {
+      labelArray.forEach(function(elem) {
+        if (elem.name == t) {
+          check = true;
+        }
+      });
+      if (check) context.log(context.payload.comment.body);
+    }
+  });
+};
