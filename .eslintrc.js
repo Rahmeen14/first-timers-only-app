8550c2841d8da4cb9ec7890869de5f6e404e4504
Akshita <aggarwalakshita1@gmail.com>
2018-07-12 23:12:36 +0530

    modify index.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "standard",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 module.exports = app => {
-  app.log("Yay, the app was loaded!");
+  app.log('Yay, the app was loaded!')
   app.on("issue_comment", async context => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
 
-    const s = "Can I work on this?";
-    const t = "first-timers-only";
-    let check = false;
-    let labelArray = context.payload.issue.labels;
-    if (context.payload.comment.body == s) {
-      labelArray.forEach(function(elem) {
-        if (elem.name == t) {
-          check = true;
+    const comparisonString = 'Can I work on this?'
+    const label = 'first-timers-only'
+    let check = false
+    const labelArray = context.payload.issue.labels
+
+    if (context.payload.comment.body === comparisonString) {
+      labelArray.forEach(function (elem) {
+        if (elem.name === label) {
+          check = true
         }
-      });
-      if (check) context.log(context.payload.comment.body);
+      })
+      if (check) context.log(context.payload.comment.body)
     }
-  });
-};
+  })
+}
