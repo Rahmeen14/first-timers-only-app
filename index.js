e99dd9e44109c5974766a7b716403e548bf1b2b6
Akshita <aggarwalakshita1@gmail.com>
2018-07-17 22:46:17 +0530

    Add test

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+const {
+  isAFirstTimersOnlyIssue,
+  isCommentAskingForClaimingTheIssue
+} = require('./util')
+
 module.exports = app => {
   app.log('Yay, the app was loaded!')
+
   app.on('issue_comment', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    if (isCommentAskingForClaimingTheIssue(context)) {
+      context.log(context.payload.issue.labels)
 
-    const comparisonString = 'Can I work on this?'
-    const label = 'first-timers-only'
-    let check = false
-    const labelArray = context.payload.issue.labels
-
-    if (context.payload.comment.body === comparisonString) {
-      labelArray.forEach(function (elem) {
-        if (elem.name === label) {
-          check = true
-        }
-      })
-      if (check) context.log(context.payload.comment.body)
+      if (isAFirstTimersOnlyIssue(context)) {
+        context.log(context.payload.comment.body)
+      }
     }
   })
 }
